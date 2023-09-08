1e202ca2f6c2fd43f42f964adb441971e8f967f2
anants2102 <anants231002@gmail.com>
2023-09-09 02:13:08 +0530

    new strat1

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -63,7 +63,7 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
