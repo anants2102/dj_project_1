523a77901f61e994dce4fe390e3c3be3ab652ac2
anants2102 <anants231002@gmail.com>
2023-09-09 16:01:44 +0530

    little change 2

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -63,8 +63,8 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
+        # 'DIRS': [BASE_DIR / 'templates'],
+        # 'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -72,6 +72,12 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+        'loaders': [
+            (
+                'django.template.loaders.filesystem.Loader',
+                [BASE_DIR / 'templates'],
+            ),
+        ]
         },
     },
 ]
